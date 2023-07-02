bdc0f6cfc004fec94d1f80dcfc5e4f87632aea8f
abc12354a <abc12354a@hotmail.com>
2023-07-02 19:48:41 +0800

    update sim

--- FPGA/MIPS/sim/soc_tb.v
+++ FPGA/MIPS/sim/soc_tb.v
@@ -9,19 +9,23 @@ module soc_tb();
  //***************************** Main Code ****************************//
  //********************************************************************//
  //初始化系统时钟、全局复位
- initial begin
- sys_clk = 1'b1;
- sys_rst_n <= 1'b0;
- #20
- sys_rst_n <= 1'b1;
- end
+    initial begin
+        sys_clk = 1'b1;
+        sys_rst_n <= 1'b0;
+        #200
+        sys_rst_n <= 1'b1;
+    end
+
+    initial begin
+        #500 $finish();
+    end
  
  //sys_clk:模拟系统时钟，每 10ns 电平翻转一次，周期为 20ns，频率为 50Mhz
- always #10 sys_clk = ~sys_clk;
+    always #10 sys_clk = ~sys_clk;
        
-  mips u_mips(
-		.clk(sys_clk),
-		.rst_n(sys_rst_n)	
-	);
+    mips u_mips(
+        .clk(sys_clk),
+        .rst_n(sys_rst_n)	
+    );
 
 endmodule
